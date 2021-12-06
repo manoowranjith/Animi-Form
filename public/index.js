4cae67cbbea8160de99212ae1d4ee00a4f011149
manoowranjith <a.j.manoow@gmail.com>
2021-12-06 10:59:05 +0000

    Animi-Form-V2

--- public/index.js
+++ public/index.js
@@ -1,27 +1,41 @@
+function errorcall_name(x)
+{
+    document.getElementById("message_name").innerHTML=x;
+}
+function errorcall_episode(x)
+{
+    document.getElementById("message_episode").innerHTML=x;
+}
 function validateForm()
 {
     const name = document.forms["myForm"]["anime-name"].value;
     const episodes=document.forms["myForm"]["epi-sode"].value;
 
     if (name == "") {
-        alert("Name should not be empty");
+        // document.getElementById("message").innerHTML="*Name should not be empty";
+        // document.getElementById("btn").disable=true;
+        // alert("Name should not be empty");
+        errorcall_name("*Name should not be empty")
         return false;
     }
     else if(name.length<8)
     {
-        alert("Name should be minimum of 8 charactes and above");
+        // alert("Name should be minimum of 8 charactes and above");
+        errorcall_name("*Name should be minimum of 8 charactes and above");
         return false;
     }
 
     else if(name.length>14)
     {
-        alert("Name should be less than 15 characters");
+        errorcall_name("*Name should be less than 15 characters")
+        // alert("Name should be less than 15 characters");
         return false;
     }
 
     else if(name.indexOf(' ') >= 0)
     {
-        alert("No space is allowed in Animi-name");
+        errorcall_name("*No space is allowed in Animi-name")
+        // alert("No space is allowed in Animi-name");
         return false;
     }
 
@@ -30,20 +44,16 @@ function validateForm()
         var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
         if(format.test(name))
         {
-            alert("Animi-name should not contain special character");
+            errorcall_name("*Animi-name should not contain special character")
+            // alert("Animi-name should not contain special character");
             return false;
         } 
     }
 
-    if(episodes == "")
+    if(!episodes.match(/^[0-9]+$/))
     {
-        alert("Number of episodes should not be empty");
-        return false;
-    }
-
-    else if(!episodes.match(/^[0-9]+$/))
-    {
-        alert("Number of episodes should only contain numbers");
+        errorcall_episode("*Number of episodes should only contain numbers")
+        // alert("Number of episodes should only contain numbers");
         return false;
     }
     
